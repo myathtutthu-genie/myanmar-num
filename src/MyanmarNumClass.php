5e73849a99a1bedec1884aea0a8f9fba783f2f41
myathtutthu-genie <myathtutthu@geniefintech.com>
2022-04-15 16:56:49 +0630

    fix: stone feature

--- src/MyanmarNumClass.php
+++ src/MyanmarNumClass.php
@@ -4,17 +4,25 @@ namespace MHT\MyanmarNum;
 
 class MyanmarNumClass
 {
+    protected float $kilograms;
+
     public static function kilograms(float $kilograms): self
     {
         return new static($kilograms);
     }
 
-    public function __construct(protected float $kilograms)
+    public function __construct(float $kilograms)
     {
+        $this->kilograms = $kilograms;
     }
 
     public function toPound(): float
     {
         return $this->kilograms * 2.2046;
+    }
+
+    public function toStone(): float
+    {
+        return $this->kilograms * 0.15747;
     }
 }
--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -7,3 +7,8 @@ it('can test', function () {
     $pounds = MyanmarNumClass::kilograms(100)->toPound();
     expect($pounds)->toEqual(220.46);
 });
+
+it('can stone', function () {
+    $pounds = MyanmarNumClass::kilograms(100)->toStone();
+    expect($pounds)->toEqual(15.747);
+});
